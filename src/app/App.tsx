00a07290d526698b7d5c474c6ee1e3c24988414c
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-05-05 20:23:17 +0300

    Add alias

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,23 @@
 import React, {Suspense, useContext, useState} from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss'
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { MainPage } from "pages/MainPage";
+import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
-  const bool = true
 
   return (
-     <div className={classNames('app', {}, [theme])}>
+     <div className={ classNames('app', {}, [theme]) }>
        <button onClick={toggleTheme}>Theme</button>
        <Link to={'/'}>Главная</Link>
        <Link to={'/about'}>О сайте</Link>
        <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<MainPageAsync />} />
-          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Suspense>
      </div>
