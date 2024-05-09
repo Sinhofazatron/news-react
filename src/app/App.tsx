1bb6f21df8c84d5c3b54c7b6d0f4a3abebbb19ab
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-05-09 11:06:49 +0300

    Add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { MainPage } from "pages/MainPage";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AboutPage } from "pages/AboutPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -14,12 +15,7 @@ const App = () => {
        <button onClick={toggleTheme}>Theme</button>
        <Link to={'/'}>Главная</Link>
        <Link to={'/about'}>О сайте</Link>
-       <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+       <AppRouter/>
      </div>
   );
 };
