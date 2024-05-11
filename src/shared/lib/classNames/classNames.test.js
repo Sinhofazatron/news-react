ee62a32ff9639c0d4fd61d1a0be12228e473037b
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-05-11 13:56:33 +0300

    Add Not Found Page

---
+++ src/shared/lib/classNames/classNames.test.js
@@ -0,0 +1,27 @@
+import { classNames } from "./classNames";
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('some')).toBe('some');
+  });
+
+  test('with additional class', () => {
+    const expected = 'some class1 class2';
+    expect(classNames('some', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'some class1 class2 hovered scroll';
+    expect(classNames('some', {hovered: true, scroll: true}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods false', () => {
+    const expected = 'some class1 class2 hovered';
+    expect(classNames('some', {hovered: true, scroll: false}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'some class1 class2 hovered';
+    expect(classNames('some', {hovered: true, scroll: undefined}, ['class1', 'class2'])).toBe(expected);
+  })
+})
