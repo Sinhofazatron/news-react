5eda4aaa8268c9378772affa3818a20b9d424a85
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-05-12 19:45:28 +0300

    Upgrade sidebar

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -3,26 +3,45 @@ import cls from './Button.module.scss'
 import {ButtonHTMLAttributes, FC} from "react";
 
 export enum ThemeButton {
-  CLEAR = 'clear'
+  CLEAR = 'clear',
+  OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+  L = 'size_l',
+  M = 'size_m',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   className?: string;
-  theme?: ThemeButton
+  theme?: ThemeButton;
+  square?: boolean;
+  size?: ButtonSize
 }
 
 export const Button: FC<ButtonProps> = (props) => {
   const {
     className,
     children,
-     theme,
+    theme,
+    square ,
+    size= ButtonSize.M,
     ...otherProps
   } = props
+
+  const mods: Record<string, boolean> = {
+    [cls[theme]]: true,
+    [cls.square]: square,
+    [cls[size]]: true,
+  }
 
   return (
      <button
         type='button'
-        className={classNames(cls.Button, {[cls[theme]]: true}, [className])}
+        className={classNames(cls.Button, mods, [className])}
         {...otherProps}
      >
        {children}
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,17 +1,19 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './NavBar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
 
 interface NavBarProps {
   className?: string;
 }
 
 export const NavBar = ({ className }: NavBarProps) => {
+  const { t } = useTranslation()
+
   return (
      <div className={classNames(cls.Navbar, {}, [className])}>
        <div className={cls.links}>
-         <AppLink to={'/'} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>Главная</AppLink>
-         <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
+         /
        </div>
      </div>
   );
