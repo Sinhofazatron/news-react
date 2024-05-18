69dbb30b6259821810bd7ca59df66ca77d846d95
Dmitry Sitnikov <betani.elvin@mail.ru>
2024-05-18 19:03:23 +0300

    Add modal and portal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import './styles/index.scss'
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
 import { NavBar } from "widgets/NavBar";
 import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Modal } from "shared/ui/Modal/Modal";
 
 const App = () => {
   const {theme } = useTheme()
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,7 +1,9 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './NavBar.module.scss'
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 import {useTranslation} from "react-i18next";
+import {Modal} from "shared/ui/Modal/Modal";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+import {useCallback, useState} from "react";
 
 interface NavBarProps {
   className?: string;
@@ -9,12 +11,24 @@ interface NavBarProps {
 
 export const NavBar = ({ className }: NavBarProps) => {
   const { t } = useTranslation()
+  const [isAuthModal, setIsAuthModal] = useState<boolean>(false)
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal(prev => !prev)
+  }, [])
 
   return (
      <div className={classNames(cls.Navbar, {}, [className])}>
-       <div className={cls.links}>
-         /
-       </div>
+       <Button
+          theme={ThemeButton.CLEAR_INVERTED}
+          className={cls.links}
+          onClick={onToggleModal}
+       >
+         {t('Войти')}
+       </Button>
+       <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+         sdfgdsglskj;dfm'lakdmg;osdfnjga;dfmg/ldfnliudafhogus
+       </Modal>
      </div>
   );
 };
